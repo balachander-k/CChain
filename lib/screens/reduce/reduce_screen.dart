dfd72475ceb11fb4184cf46e7e8e54ccb7f7aa9d
AvinashIT <117062802+dinesharumugam8@users.noreply.github.com>
2024-02-20 11:31:39 +0530

    Version_1

---
+++ lib/screens/reduce/reduce_screen.dart
@@ -0,0 +1,218 @@
+import 'package:CCHAIN/helpers/colors.dart';
+import 'package:CCHAIN/helpers/text_theme.dart';
+import 'package:CCHAIN/screens/home/startScreen.dart';
+import 'package:flutter/material.dart';
+
+class ReduceEmissionScreen extends StatelessWidget {
+  static const routeName = "/reduce-carbon-footprint";
+
+  List<Widget> getWidgetTree() {
+    final List reduceCarbonFootPrintmsgs = [
+      {
+        'icon': Icons.hot_tub,
+        'title': 'Reduce emissions due to household activities',
+        'options': [
+          {
+            'icon': Icons.do_not_disturb_alt,
+            'message': 'Do not forget to switch off the lights or unplug your electronic devices when they are not in use',
+          },
+          {
+            'icon': Icons.done_outline,
+            'message': 'Lower the amount of energy used to pump,treat and heat water by washing your car less often, using climate-appropriate plants in garden',
+          },
+          {
+            'icon': Icons.do_not_disturb_alt,
+            'message': "Don't set thermostat too high or low. Install a programmable model to turn off the heat or air conditioning when you're not at home",
+          },
+        ],
+      },
+      {
+        'icon': Icons.card_travel,
+        'title': 'Reduce emissions due to your commutes',
+        'options': [
+          {
+            'icon': Icons.do_not_disturb_alt,
+            'message': 'Do not unnecessarily speed up or accelerate,it increases the mileage upto 33%, waste gas, money and increases carbon emission',
+          },
+          {
+            'icon': Icons.done_outline,
+            'message': 'When possible,walk or ride your bike in order to avoid carbon emission completely',
+          },
+          {
+            'icon': Icons.do_not_disturb_alt,
+            'message': "Don't buy a minivan or SUV if you don't need 4WD and/or will ocassionally need extra space",
+          },
+        ],
+      },
+      {
+        'icon': Icons.fastfood,
+        'title': 'Reduce emissions due to food activities',
+        'options': [
+          {
+            'icon': Icons.do_not_disturb_alt,
+            'message': 'Stop wasting food!',
+          },
+          {
+            'icon': Icons.done_outline,
+            'message': 'Eat low on the food chain',
+          },
+          {
+            'icon': Icons.do_not_disturb_alt,
+            'message': "Don't eat excess calories!",
+          },
+        ],
+      },
+    ];
+
+    final List<Widget> parentWidgetTree = [];
+
+    for (final reduceEmissionData in reduceCarbonFootPrintmsgs) {
+      List<Widget> widgetTree = [];
+
+      widgetTree.add(
+        const SizedBox(
+          height: 15,
+        ),
+      );
+
+      final Widget cardHeader = ListTile(
+        leading: Icon(
+          reduceEmissionData['icon'],
+          color: ColorPallete.color3,
+        ),
+        title: CoolText(
+          reduceEmissionData['title'],
+          fontSize: 17, letterSpacing: 0,
+        ),
+      );
+
+      widgetTree.add(cardHeader);
+
+      final List<Widget> sectionDivider = [
+        const SizedBox(
+          height: 8,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Divider(
+            thickness: 1,
+            color: ColorPallete.color6,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+      ];
+
+      widgetTree = [...widgetTree, ...sectionDivider];
+
+      for (final option in reduceEmissionData['options']) {
+        final Widget optionWidget = ListTile(
+          leading: Icon(
+            option['icon'],
+            color: ColorPallete.color3,
+          ),
+          title: Text(
+            option['message'],
+            style: const TextStyle(
+              color: ColorPallete.color3,
+            ),
+          ),
+        );
+
+        widgetTree.add(optionWidget);
+      }
+
+      widgetTree.add(
+        const SizedBox(
+          height: 10,
+        ),
+      );
+
+      final Widget parentWidget = Container(
+        padding: const EdgeInsets.all(8),
+        width: double.infinity,
+        child: Card(
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          color: ColorPallete.cardBackground,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: widgetTree,
+            ),
+          ),
+        ),
+      );
+
+      parentWidgetTree.add(parentWidget);
+    }
+
+    return parentWidgetTree;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorPallete.background,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Container(
+            //   color: ColorPallete.cardBackground,
+            //   child: SizedBox(
+            //     height: MediaQuery.of(context).size.height / 6,
+            //     width: double.infinity,
+            //     child: Column(
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       children: [
+            //         Padding(
+            //           padding: const EdgeInsets.all(10),
+            //           child: CoolText(
+            //             "Reduce your carbon footprint",
+            //             fontSize: 18,
+            //             letterSpacing: 1.1,
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...getWidgetTree(),
+            const SizedBox(
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        backgroundColor: ColorPallete.cardBackground.withBlue(150),
+        onPressed: () {
+          Navigator.pushNamed(
+            context,
+            StartScreen.routeName,
+          );
+        },
+        label: const Text(
+          "Home",
+          style: TextStyle(
+            color: ColorPallete.color3,
+          ),
+        ),
+        icon: const Icon(
+          Icons.home,
+          color: ColorPallete.color3,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/result/result_screen.dart
@@ -0,0 +1,311 @@
+import 'package:CCHAIN/helpers/colors.dart';
+import 'package:CCHAIN/helpers/text_theme.dart';
+import 'package:CCHAIN/screens/reduce/reduce_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_xlider/flutter_xlider.dart';
+
+class ResultScreen extends StatelessWidget {
+  final double userEmission;
+  final double averageEmission;
+  final String activityName;
+
+  static const routeName = "/result";
+
+  ResultScreen(
+      {required Key key, required this.activityName, required this.userEmission, required this.averageEmission,})
+      : super(key: key);
+
+  Text toolTipLabel = const Text(
+    " tonnes CO2",
+    style: TextStyle(
+      fontSize: 30,
+      color: ColorPallete.color3,
+    ),
+  );
+
+  double roundOff(double value) {
+    return double.parse(value.toStringAsFixed(2));
+  }
+
+  // returns the percentage change between
+  // the emission by user and average emission of that category
+  double getChangePercentage() {
+    double changePercent =
+        ((userEmission - averageEmission) / averageEmission) * 100;
+
+    changePercent = changePercent >= 0 ? changePercent : -changePercent;
+
+    return roundOff(changePercent);
+  }
+
+  List<Widget> getDataGrids(String changePrefix, double changePercent) {
+    final List<Map> emissionDatas = [
+      {
+        'text': 'Your carbon footprint for $activityName activities',
+        'figure': '${roundOff(userEmission)} tonnes CO2',
+        'icon': Icons.person_outline,
+      },
+      {
+        'text': 'Average carbon footprint for $activityName',
+        'figure': '${roundOff(averageEmission)} tonnes CO2',
+        'icon': Icons.nature_people,
+      },
+      {
+        'text': 'You are emitting $changePrefix emission than average',
+        'figure': '$changePercent %',
+        'icon': Icons.track_changes,
+      },
+    ];
+
+    final List<Widget> widgetList = [];
+
+    for (final emissionData in emissionDatas) {
+      final Widget listItem = Card(
+        elevation: 2,
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        color: ColorPallete.cardBackground,
+        child: ListTile(
+          title: CoolText(
+            emissionData['figure'],
+            fontSize: 18,
+            letterSpacing: 1.1,
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+            child: Text(
+              emissionData['text'],
+              style: const TextStyle(
+                color: ColorPallete.color7,
+                fontSize: 13,
+                letterSpacing: 0.7,
+              ),
+            ),
+          ),
+          trailing: Icon(
+            emissionData['icon'],
+            color: ColorPallete.color3,
+          ),
+        ),
+      );
+
+      widgetList.add(listItem);
+    }
+
+    return widgetList;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    FlutterSliderHandler leftHandler;
+    FlutterSliderHandler rightHandler;
+
+    final FlutterSliderHandler userHandler = FlutterSliderHandler(
+      child: Material(
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(7),
+        ),
+        color: ColorPallete.color4,
+        elevation: 3,
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          child: const Icon(
+            Icons.person,
+            size: 25,
+            color: ColorPallete.color3,
+          ),
+        ),
+      ),
+    );
+
+    final FlutterSliderHandler avgHandler = FlutterSliderHandler(
+      child: Material(
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(7),
+        ),
+        // type: MaterialType.canvas,
+        color: ColorPallete.color3,
+        shadowColor: ColorPallete.cardBackground,
+        elevation: 3,
+        child: Container(
+          padding: const EdgeInsets.all(5),
+          child: const Icon(
+            Icons.nature_people,
+            size: 25,
+            color: ColorPallete.color4,
+          ),
+        ),
+      ),
+    );
+
+    List<double> values = [roundOff(userEmission), roundOff(averageEmission)];
+    Color trackBarColor = Colors.greenAccent;
+    leftHandler = userHandler;
+    rightHandler = avgHandler;
+    String changePercentageSuffix = "less";
+
+    final double changePercent = getChangePercentage();
+
+    if (userEmission > averageEmission) {
+      values = [roundOff(averageEmission), roundOff(userEmission)];
+      trackBarColor = Colors.red;
+      leftHandler = avgHandler;
+      rightHandler = userHandler;
+      changePercentageSuffix = "more";
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: ColorPallete.cardBackground,
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            color: ColorPallete.color7,
+          ),
+          title: Text(
+            "$activityName carbon footprint",
+            style: const TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 16,
+                color: ColorPallete.color7,),
+          ),
+        ),
+        backgroundColor: ColorPallete.background,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: ColorPallete.cardBackground,
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CoolText(
+                        "${roundOff(userEmission)} tonnes CO2",
+                        fontSize: 22,
+                        letterSpacing: 1.1,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        "Your carbon footprint is $changePercent% $changePercentageSuffix than an average person",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: ColorPallete.color7,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  children: <Widget>[
+                    Card(
+                      elevation: 2,
+                      shape: BeveledRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      color: ColorPallete.cardBackground,
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: FlutterSlider(
+                          step: const FlutterSliderStep(step: 0.01),
+                          values: values,
+                          rangeSlider: true,
+                          disabled: true,
+                          min: 0,
+                          // Set a max limit according to the highest value between `userEmission` & `avgEmission`
+                          max: values[1] + values[1] / 10,
+                          trackBar: FlutterSliderTrackBar(
+                              activeDisabledTrackBarColor: trackBarColor,
+                              inactiveDisabledTrackBarColor:
+                                  ColorPallete.color6,),
+                          handler: leftHandler,
+                          rightHandler: rightHandler,
+                          tooltip: FlutterSliderTooltip(
+                            positionOffset:
+                                FlutterSliderTooltipPositionOffset(top: -5),
+                            boxStyle: const FlutterSliderTooltipBox(
+                              decoration: BoxDecoration(
+                                color: ColorPallete.cardBackground,
+                              ),
+                            ),
+                            alwaysShowTooltip: true,
+                            textStyle: const TextStyle(
+                              fontSize: 60,
+                              color: Colors.white,
+                            ),
+                            leftSuffix: toolTipLabel,
+                            rightSuffix: toolTipLabel,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ...getDataGrids(
+                      changePercentageSuffix,
+                      changePercent,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Divider(
+                      thickness: 0.7,
+                      color: ColorPallete.color6,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Card(
+                      color: ColorPallete.cardBackground,
+                      shape: BeveledRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.pushNamed(
+                              context, ReduceEmissionScreen.routeName,);
+                        },
+                        title: const CoolText(
+                          "Reduce carbon emissions",
+                          fontSize: 18,
+                          letterSpacing: 1.1,
+                        ),
+                        subtitle: const Padding(
+                          padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
+                          child: Text(
+                            "Our earth needs it. Let's move together in this.",
+                            style: TextStyle(
+                              color: ColorPallete.color7,
+                              fontSize: 13,
+                              letterSpacing: 0.7,
+                            ),
+                          ),
+                        ),
+                        trailing: const Icon(
+                          Icons.nature,
+                          color: ColorPallete.color3,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
